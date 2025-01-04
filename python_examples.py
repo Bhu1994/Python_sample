c60ead1350043528337aee1ffe4c992d4fe2611c
Bhushan Waje <95437928+Bhu1994@users.noreply.github.com>
2025-01-04 20:52:45 +0530

    latest code

--- python_examples.py
+++ python_examples.py
@@ -1,1 +1,15 @@
-print('share \'mira' + 'cle\'')
+b='!!!!dadfd!!!! kkkk'
+
+print(len(b))
+print(b[0:3])
+print(b.upper())
+print(b.lower())
+print(b.rstrip('!'))
+print(b.lstrip('!'))
+
+print(b.replace('d','f'))
+print(b.split(' '))
+
+# for i in b:
+#     print(i)
+
